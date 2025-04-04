3c4d551d740cd0750f8e099cab68eb53e2f62aec
Sarvesh Palav <sarveshpalav@gmail.com>
2025-04-04 18:57:28 +0530

    O(n) examples added

---
+++ O(n).py
@@ -0,0 +1,29 @@
+# O(n)
+
+def print_items(n):
+    for i in range(n):
+        print(i)
+
+print_items(10)
+
+
+#O(n^2)
+
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print(i,j) 
+
+print_items(10)
+
+
+           
+              
+                  
+                  
+
+
+
+
+
+
